820855172061e19f556ea04150dc49e172483446
Naveen S <168709832+Naveen4421@users.noreply.github.com>
2026-03-09 20:52:42 +0530

    Add isAnagram function to check for anagrams

---
+++ anagram.py
@@ -0,0 +1,8 @@
+def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        from collections import Counter
+        return Counter(s)==Counter(t)
