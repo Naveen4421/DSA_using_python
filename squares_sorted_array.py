3e12c8e8edf973a73db67863c71821da20819363
Naveen S <168709832+Naveen4421@users.noreply.github.com>
2026-03-16 19:35:00 +0530

    Implement function to return sorted squares of array

---
+++ squares_sorted_array.py
@@ -0,0 +1,9 @@
+ def sortedSquares(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            nums[i]=nums[i]*nums[i]
+        nums=sorted(nums)
+        return nums
