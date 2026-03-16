121355cef1e313479361e74aad297575833f2899
Naveen S <168709832+Naveen4421@users.noreply.github.com>
2026-03-16 20:21:18 +0530

    Add function to remove duplicates from linked list

---
+++ remove_duplicate.py
@@ -0,0 +1,12 @@
+ def deleteDuplicates(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        curr=head
+        while(curr and curr.next):
+            if curr.val==curr.next.val:
+                curr.next=curr.next.next
+            else:
+                curr=curr.next
+        return head
