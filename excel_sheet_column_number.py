94c139924c3366c8bffc8ec7e5dfe8dd02916d19
Naveen S <168709832+Naveen4421@users.noreply.github.com>
2026-04-12 17:08:03 +0530

    Implement function to convert Excel column title to number

---
+++ excel_sheet_column_number.py
@@ -0,0 +1,10 @@
+def titleToNumber(self, columnTitle):
+        """
+        :type columnTitle: str
+        :rtype: int
+        """
+        result=0
+        for c in columnTitle:
+            value=ord(c)-ord("A")+1
+            result=result*26+value
+        return result
