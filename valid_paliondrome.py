64c8f5ab7f3adc4d63ae8b0501cf65672a750ddf
Naveen S <168709832+Naveen4421@users.noreply.github.com>
2026-04-12 17:09:00 +0530

    Implement palindrome checker function

---
+++ valid_paliondrome.py
@@ -0,0 +1,10 @@
+def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        c=""
+        for ch in s:
+            if ch.isalnum():
+                c+=ch.lower()
+        return c==c[::-1]
