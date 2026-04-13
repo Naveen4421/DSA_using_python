930698f45d2e8abb3d5db4927a1c560b8bae1b40
Naveen S <168709832+Naveen4421@users.noreply.github.com>
2026-04-13 20:28:56 +0530

    Implement function to find minimum distance to target

---
+++ minimum_distance_to_target.py
@@ -0,0 +1,12 @@
+def getMinDistance(self, nums, target, start):
+        """
+        :type nums: List[int]
+        :type target: int
+        :type start: int
+        :rtype: int
+        """
+        min_dist = float('inf')
+        for i in range(len(nums)):
+            if nums[i]==target:
+               min_dist=min(min_dist,abs(i-start))
+        return min_dist
