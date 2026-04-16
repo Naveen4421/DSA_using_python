8b394361d00b96e8489073c715ad0d10c6b43b7b
Naveen S <168709832+Naveen4421@users.noreply.github.com>
2026-04-16 20:40:26 +0530

    Implement first unique character function

---
+++ first_unique_element.py
@@ -0,0 +1,12 @@
+def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        seen={}
+        for i,c in enumerate(s):
+            seen[c]=seen.get(c,0)+1
+        for i,c in enumerate(s):
+            if seen[c]==1:
+                return i
+        return -1
