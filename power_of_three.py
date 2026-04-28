29fd9abc7f256a4b050329ed8f45bebf89a862c6
Naveen S <168709832+Naveen4421@users.noreply.github.com>
2026-04-28 19:37:21 +0530

    Add function to check if a number is a power of three

---
+++ power_of_three.py
@@ -0,0 +1,10 @@
+def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n<=0:
+            return False
+        while n%3==0:
+            n=n//3
+        return n==1
