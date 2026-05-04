f8437777f971371ac6e97ab868c1243582c4e90a
Naveen S <168709832+Naveen4421@users.noreply.github.com>
2026-05-04 23:03:49 +0530

    Implement cloning of a linked list with random pointers

---
+++ cloning_linked_list.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def copyRandomList(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
+        if not head:
+            return None
+        node_map={}
+        curr=head
+        while curr:
+            node_map[curr]=Node(curr.val)
+            curr=curr.next
+        
+        curr=head
+        while curr:
+            if curr.next:
+                node_map[curr].next=node_map[curr.next]
+            if curr.random:
+                node_map[curr].random=node_map[curr.random]
+            curr=curr.next
+        return node_map[head]
