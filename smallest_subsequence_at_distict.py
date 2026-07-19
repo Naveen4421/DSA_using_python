baefc0e2100daa05eb98d6fd0ef857deefd946dd
Naveen S <168709832+Naveen4421@users.noreply.github.com>
2026-07-19 21:53:17 +0530

    Create smallest_subsequence_at_distict.py

---
+++ smallest_subsequence_at_distict.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def smallestSubsequence(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        last_occurrence = {c: i for i, c in enumerate(s)}
+        stack = []
+        seen = set()
+
+        for i, c in enumerate(s):
+            if c in seen:
+                continue
+            while stack and stack[-1] > c and last_occurrence[stack[-1]] > i:
+                seen.remove(stack.pop())
+            stack.append(c)
+            seen.add(c)
+
+        return ''.join(stack)
+        
