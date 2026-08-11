c143dd3404edfeda1b413f1b4d204ec28457b53d
Naveen S <168709832+Naveen4421@users.noreply.github.com>
2026-08-12 00:05:41 +0530

    Add nextPermutation method to Solution class
    Implement next permutation algorithm to modify nums in-place.

---
+++ next_permutation.py
@@ -0,0 +1,30 @@
+
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n=len(nums)
+        pivot=-1
+        #find pivot element
+        for i in range(n-2,-1,-1):
+            if nums[i]<nums[i+1]:
+                pivot=i
+                break
+        # swap elements
+
+        if pivot!=-1:
+            for i in range(n-1,pivot,-1):
+                if nums[i]>nums[pivot]:
+                    nums[i],nums[pivot]=nums[pivot],nums[i]
+                    break
+        
+        # reverse elemet swap
+
+        left,right=pivot+1,n-1
+        while left<right:
+            nums[left],nums[right]=nums[right],nums[left]
+            left+=1
+            right-=1
+        return nums
